6722cd3905d44d2af0776747cdb2d28451ffb75c
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-04 23:03:30 +0400

    login maked

--- bookshoop/src/App.js
+++ bookshoop/src/App.js
@@ -1,12 +1,14 @@
 import {Route,Routes} from "react-router-dom"
 import './App.css';
 import Login from "./components/Login/Login";
+import Admin from "./components/AdminDashboard/Admin";
 
 function App() {
   return (
     <div className="App">
 <Routes>
   <Route path="/" element={<Login/>}/>
+  <Route path="/admin" element={<Admin/>}/>
 </Routes>
   
     </div>
--- bookshoop/src/components/Login/Login.jsx
+++ bookshoop/src/components/Login/Login.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.scss";
 import logo from "../../images/logo.png";
+import axios from "axios";
+import { useState } from "react";
+const BASE_URL = "http://localhost:3000/admin";
+
 const Login = () => {
+  const navigate=useNavigate()
+  const [arr, setArr] = useState([]);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  //data cekme
+  const fetchData = async () => {
+    let res = await axios.get(BASE_URL);
+    let data = res.data;
+    setArr(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handkeClick = (e) => {
+    e.preventDefault();
+    arr.map((admin) => {
+      console.log(admin.userName);
+      console.log(admin.password);
+      console.log(name);
+      console.log(password);
+    
+      if (name == admin.userName && password == admin.password) {
+       navigate('/admin')
+      } else {
+        console.log("---");
+      }
+    });
+  };
+
   return (
     <div className="parent-div">
-      <div className="main-div"> 
+      <div className="main-div">
         <form className="form">
           <div className="title-side">
             <img className="logo" src={logo} alt="" />
@@ -15,12 +50,21 @@ const Login = () => {
             </div>
           </div>
           <h3 className="heading">Alfresco Identity Service</h3>
-          <div className="inputs"> 
-          <input type="text" placeholder="Username:" />
-          <input type="password" placeholder="Password" />
-          <button>Sign in</button>
+          <div className="inputs">
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Username:"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password:"
+            />
+            <button onClick={handkeClick}>Sign in</button>
           </div>
-        
         </form>
       </div>
     </div>
